9128edebe35988583b2e3b870f00054076344aca
Roy-Hu <r74777477@gmail.com>
2024-02-09 17:07:19 -0600

    Error handling

--- samples/montty.c
+++ samples/montty.c
@@ -10,13 +10,17 @@
 #define BUF_SIZE 512  
 
 static bool isInit[NUM_TERMINALS];
+static bool isInitMonitor = false;
+
 static int num_readers[NUM_TERMINALS], num_writers[NUM_TERMINALS];
 
 // lock for the driver
-static cond_id_t reader[NUM_TERMINALS], writer[NUM_TERMINALS], read[NUM_TERMINALS];
+static cond_id_t reader[NUM_TERMINALS], writer[NUM_TERMINALS], read[NUM_TERMINALS], last_write[NUM_TERMINALS];
 
 // lock for the hardware
-static cond_id_t echo, write;
+static cond_id_t write;
+
+int num_last_write[NUM_TERMINALS];
 
 static int num_echo, num_write, num_stas;
 
@@ -30,12 +34,13 @@ static int newline_num[NUM_TERMINALS];
 static char *echo_buf[NUM_TERMINALS];
 static int echo_in[NUM_TERMINALS], echo_out[NUM_TERMINALS], echo_count[NUM_TERMINALS];
 
+
 static void
 addItem(char *buf, int *in, int *cnt, char c)
 {
     buf[*in] = c;
 
-    (*cnt)++;
+    *cnt = *cnt + 1;
 
     *in = (*in + 1) % BUF_SIZE;
 }
@@ -50,7 +55,7 @@ removeItem(char *buf, int *out, int *cnt)
 
     char c = buf[*out];
     
-    (*cnt)--;
+    *cnt = *cnt - 1;
 
     *out = (*out + 1) % BUF_SIZE;
 
@@ -86,9 +91,8 @@ ReceiveInterrupt(int term)
     if (c == '\177') c = '\b';
 
     if (c == '\b') {
-        if (read_count[term] > 0) {
-            removeItem(read_buf[term], &read_out[term], &read_count[term]);
-        } 
+        read_in[term]--;
+        read_count[term]--;
     } else if (read_count[term] < BUF_SIZE) {
         addItem(read_buf[term], &read_in[term], &read_count[term], c);
 
@@ -108,12 +112,11 @@ ReceiveInterrupt(int term)
     } else if (c == '\n') {
         addItem(echo_buf[term], &echo_in[term], &echo_count[term], '\r');
         addItem(echo_buf[term], &echo_in[term], &echo_count[term], '\n');
-
     } else {
         addItem(echo_buf[term], &echo_in[term], &echo_count[term], c);
     }
 
-
+    // Directly write to the terminal if no need to wait
     if (num_echo == 0 && num_write == 0) {
         num_echo++;
 
@@ -125,6 +128,23 @@ extern int
 ReadTerminal(int term, char *buf, int buflen)
 {
     Declare_Monitor_Entry_Procedure();
+    
+    if (term < 0 || term >= NUM_TERMINALS) {
+        printf("[ERROR]: Invalid terminal number %d\n", term);
+        return -1;
+    }
+
+    if (!isInitMonitor || !isInit[term]) {
+        printf("[ERROR]: Terminal %d is not initialized\n", term);
+        return -1;
+    }
+
+    if (buf == NULL) {
+        printf("[ERROR]: Buf is NULL\n");
+        return -1;
+    }
+
+    if (buflen == 0) return 0;
 
     while (num_readers[term] > 0) CondWait(reader[term]);
 
@@ -134,7 +154,6 @@ ReadTerminal(int term, char *buf, int buflen)
     // Read from the read_buf if newline is found or buflen is reached
     while (cnt < buflen) {
         char c = removeItem(read_buf[term], &read_out[term], &read_count[term]);
-
         if (c == '\0') {
             CondWait(read[term]);
         } else {
@@ -172,6 +191,7 @@ TransmitInterrupt(int term)
         tty_out[term]++;
     }
 
+
     if (num_echo > 1 || num_write > 1 || (num_echo > 0 && num_write > 0)) {
         fprintf(stderr, "[ERROR]: Multiple write at the same time, Echo: %d, write: %d",num_echo, num_write);
         exit(0);
@@ -185,8 +205,13 @@ TransmitInterrupt(int term)
     if (echo_count[term] > 0) {
         num_echo++;
 
-        WriteDataRegister(term, removeItem(echo_buf[term], &echo_out[term], &echo_count[term]));
+        char c = removeItem(echo_buf[term], &echo_out[term], &echo_count[term]);
+        WriteDataRegister(term, c);
     } else {
+        if (num_last_write[term] > 0) {
+            num_last_write[term]--;
+            CondSignal(last_write[term]);
+        }
         // If there are no echo waiting, wake up the writer
         CondSignal(write);
     }
@@ -197,13 +222,32 @@ WriteTerminal(int term, char *buf, int buflen)
 {
     Declare_Monitor_Entry_Procedure();
 
+    if (term < 0 || term >= NUM_TERMINALS) {
+        printf("[ERROR]: Invalid terminal number %d\n", term);
+        return -1;
+    }
+    
+    if (!isInitMonitor || !isInit[term]) {
+        printf("[ERROR]: Terminal %d is not initialized\n", term);
+        return -1;
+    }
+
+    if (buf == NULL) {
+        printf("[ERROR]: Buf is NULL\n");
+        return -1;
+    }
+    
     while (num_writers[term] > 0) CondWait(writer[term]);
     
     num_writers[term]++;
 
     int write_char_num = 0;
 
-    for (int i = 0; i < buflen; i++) {        
+    for (int i = 0; i < buflen; i++) {      
+        if (i == buflen - 1) {
+            num_last_write[term]++;
+        }
+
         if (buf[i] == '\n') {
             SafeWriteReg(term, '\r');
             SafeWriteReg(term, '\n');
@@ -213,6 +257,13 @@ WriteTerminal(int term, char *buf, int buflen)
 
         write_char_num++;
     }
+
+    // Wait for the last write to finish
+    while (num_last_write[term] > 0) {
+        CondWait(last_write[term]);
+    }
+
+    num_last_write[term] = false;
 
     num_writers[term]--;
 
@@ -234,6 +285,9 @@ InitTerminal(int term)
 
     InitHardware(term);
     
+    if (!isInitMonitor) return -1;
+    if (isInit[term]) return -1;
+
     isInit[term] = true;
 
     read_buf[term] = (char *)malloc(BUF_SIZE * sizeof(char));
@@ -253,12 +307,19 @@ InitTerminal(int term)
     reader[term] = CondCreate();
     writer[term] = CondCreate();
     read[term] = CondCreate();
+    last_write[term] = CondCreate();
+    num_last_write[term] = 0;
 
     tty_in[term] = 0;
     tty_out[term] = 0;
     user_in[term] = 0;
     user_out[term] = 0;
 
+    tty_in_tmp[term] = 0;
+    tty_out_tmp[term] = 0;
+    user_in_tmp[term] = 0;
+    user_out_tmp[term] = 0;
+    
     return 0;
 }
 
@@ -267,15 +328,19 @@ InitTerminalDriver(void)
 {
     Declare_Monitor_Entry_Procedure();
 
-    echo = CondCreate();
     write = CondCreate();
 
     num_echo = 0;
     num_write = 0;
     
+    if (isInitMonitor) return -1;
+
+    isInitMonitor = true;
+
     for (int i = 0; i < NUM_TERMINALS; i++) {
         isInit[i] = false;
     }
+
 
     return 0;
 }
@@ -284,6 +349,11 @@ extern int
 TerminalDriverStatistics(struct termstat *stats)
 {
     Declare_Monitor_Entry_Procedure();
+    
+    if (stats == NULL) {
+        printf("[ERROR]: stats is NULL\n");
+        return -1;
+    }
 
     num_stas++;
 
